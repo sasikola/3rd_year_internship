152dc86877908dca0f3db506ba2edf3af3939125
sasikiran0 <sasikran@bloomitsolution.in>
2024-09-02 20:17:03 +0530

    First React app

---
+++ batch-1/src/Data/data.jsx
@@ -0,0 +1,55 @@
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaGithubSquare } from "react-icons/fa";
+
+export const links = [
+  {
+    id: 1,
+    url: "/",
+    text: "home",
+  },
+  {
+    id: 2,
+    url: "/about",
+    text: "about",
+  },
+  {
+    id: 3,
+    url: "/projects",
+    text: "projects",
+  },
+  {
+    id: 4,
+    url: "/contact",
+    text: "contact",
+  },
+  {
+    id: 5,
+    url: "/profile",
+    text: "profile",
+  },
+];
+
+export const social = [
+  {
+    id: 1,
+    url: "https://www.twitter.com",
+    icon: <FaInstagram />,
+  },
+  {
+    id: 2,
+    url: "https://www.twitter.com",
+    icon: <FaLinkedin />,
+  },
+  {
+    id: 3,
+    url: "https://www.twitter.com",
+    icon: <FaXTwitter />,
+  },
+  {
+    id: 4,
+    url: "https://www.twitter.com",
+    icon: <FaGithubSquare />,
+  },
+];
